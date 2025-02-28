a2bb2c1a18f2b55bf07b52fd8b39ab9b9563cb9f
Aditya Kashid <aditya.p.kashid@gmail.com>
2025-02-28 15:34:31 +0530

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent actual form submission
-    
-    // Redirect to the welcome page after clicking Login
-    window.location.href = "welcome.html";
-});
